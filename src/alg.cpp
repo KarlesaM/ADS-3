dd82f8373c9367e5f0b739814779d5678827dc67
KarlesaM <99256703+KarlesaM@users.noreply.github.com>
2022-05-23 22:59:04 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,9 +3,22 @@
 int cbinsearch(int *arr, int size, int value) {
   //  поместить сюда реализацию алгоритма
   return 0; // если ничего не найдено
-  int c = 0;
-  for (int a = 0; a < size; a++)
-    if (arr[a] == value)
-      c++;
-  return c;
+int a = 0, b = size - 1, middle, n = 0;
+while (a < b) {
+middle = (a + b) / 2;
+if (arr[middle] < value)
+a = middle + 1;
+else
+b = middle;
+}
+if (arr[a] == value) {
+while (arr[a] == value) {
+n++;
+a++;
+}
+}
+if (n)
+return n;
+else
+return 0; 
 }
